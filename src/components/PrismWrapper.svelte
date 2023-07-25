a42e532f7c009cbb22f5aa101ab88ff2c22009ee
Nhan Nguyen <nhan.nguyen@uac.edu.au>
2023-07-25 13:51:15 +0700

    fix

--- src/components/PrismWrapper.svelte
+++ src/components/PrismWrapper.svelte
@@ -4,8 +4,9 @@
   import 'prismjs/components/prism-json5';
   import 'prismjs/components/prism-bash';
   import 'prismjs/components/prism-dart';
-  import 'prismjs/components/prism-tsx';
   import 'prismjs/components/prism-typescript';
+  import 'prismjs/components/prism-jsx';
+  import 'prismjs/components/prism-tsx';
 
   export let className = '';
   export let classLabel = '';
